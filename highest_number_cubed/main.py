be84ac2e3edfa4072bdadaed0ad2a46129012973
Shamika Goddard <sg3147@utsnyc.edu>
2017-01-13 01:23:59 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,10 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    answer = 0
+    for num in range(limit):
+        if num**3 > limit:
+            answer = num - 1
+            return answer
+            
+print(highest_number_cubed(30))
